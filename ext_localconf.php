b28907d92df39c1430bb8009bb353282c8fcd906
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2014-06-19 15:56:30 +0200

    Explain what I am doing.

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,11 +3,13 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+/* Declarating the Twig Template cObj */
 $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_content.php']['cObjTypeAndClass'][\JX\Twypo\TwigContentObjectRenderer::CONTENT_OBJECT_NAME] = array(
 	\JX\Twypo\TwigContentObjectRenderer::CONTENT_OBJECT_NAME,
 	'\JX\Twypo\TwigContentObjectRenderer'
 );
 
+/* Override all the cObjs! */
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Frontend\\ContentObject\\Menu\\TextMenuContentObject'] = array(
     'className' => 'JX\\Twypo\\Xclass\\Menu\\TextMenuContentObject',
 );
