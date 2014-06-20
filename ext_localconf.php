608f28182ebd7bf838d6e6ca7dc1222e8589d3bc
Julian Xhokaxhiu <jx@cron.eu>
2014-06-20 13:47:22 +0200

    Add two more content objects.

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,11 +4,18 @@ if (!defined('TYPO3_MODE')) {
 }
 
 /* Declarating the Twig Template cObj */
+$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_content.php']['cObjTypeAndClass'][\JX\Twypo\TwigPageObjectRenderer::CONTENT_OBJECT_NAME] = array(
+	\JX\Twypo\TwigPageObjectRenderer::CONTENT_OBJECT_NAME,
+	'\JX\Twypo\TwigPageObjectRenderer'
+);
 $TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_content.php']['cObjTypeAndClass'][\JX\Twypo\TwigContentObjectRenderer::CONTENT_OBJECT_NAME] = array(
 	\JX\Twypo\TwigContentObjectRenderer::CONTENT_OBJECT_NAME,
 	'\JX\Twypo\TwigContentObjectRenderer'
 );
-
+$TYPO3_CONF_VARS['SC_OPTIONS']['tslib/class.tslib_content.php']['cObjTypeAndClass'][\JX\Twypo\TwigColumnMapperObjectRender::CONTENT_OBJECT_NAME] = array(
+	\JX\Twypo\TwigColumnMapperObjectRender::CONTENT_OBJECT_NAME,
+	'\JX\Twypo\TwigColumnMapperObjectRender'
+);
 /* Override all the cObjs! */
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Frontend\\ContentObject\\Menu\\TextMenuContentObject'] = array(
     'className' => 'JX\\Twypo\\Xclass\\Menu\\TextMenuContentObject',
