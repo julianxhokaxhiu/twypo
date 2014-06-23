c4cec0cbb505ffb1a7e22dd222793c7668c0a60d
Julian Xhokaxhiu <jx@cron.eu>
2014-06-23 13:22:49 +0200

    Better naming for the TS Setup file in the backend.

--- ext_tables.php
+++ ext_tables.php
@@ -1,4 +1,4 @@
 <?php
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,"Configuration/TypoScript/","Twypo: Default Setup");
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY,"Configuration/TypoScript/","Twypo: Framework Setup");
