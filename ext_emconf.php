e391653915182a1e36301ea1a3bd37df551ab15f
Julian Xhokaxhiu <jx@cron.eu>
2014-07-03 11:48:01 +0200

    Remove php requirement and bump to version 0.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,10 +29,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.0.1',
+	'version' => '0.0.2',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.4.0-0.0.0',
 			'typo3' => '6.2.0-6.2.99'
 		),
 		'conflicts' => array(
